ea4ad404343efbe74e68a1913cfb891b9f50cdbd
corvinus-klapc6 <daniel.barabas@stud.uni-corvinus.hu>
2021-10-08 19:12:51 +0200

    Negyedik feladat

--- 4.hét/4.hét/Form1.cs
+++ 4.hét/4.hét/Form1.cs
@@ -12,9 +12,18 @@ namespace _4.hét
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
