2e851286a4ff4e199aa89e111fb18f9e86da3428
corvinus-klapc6 <daniel.barabas@stud.uni-corvinus.hu>
2021-11-15 22:03:56 +0100

    Lista létrehozása és datagridview létrehozása

---
+++ KLAPC6_7hét/KLAPC6_7hét/Entities/RateData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLAPC6_7hét.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+
+        public  decimal Value { get; set; }
+    }
+}
--- KLAPC6_7hét/KLAPC6_7hét/Form1.cs
+++ KLAPC6_7hét/KLAPC6_7hét/Form1.cs
@@ -8,15 +8,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using KLAPC6_7hét.Entities;
 
 namespace KLAPC6_7hét
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             HarmadikFeladat();
+            dataGridView1.DataSource = Rates;
         }
         public void HarmadikFeladat()
         {
@@ -36,6 +39,12 @@ namespace KLAPC6_7hét
             // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
             // Ezért a result változó valójában string típusú.
             var result = response.GetExchangeRatesResult;
+            SaveFileDialog sfd = new SaveFileDialog();
+            //if (sfd.ShowDialog() != DialogResult.OK)
+            //{
+            //    return;
+            //}
+            //Console.WriteLine(result);
         }
     }
 }
